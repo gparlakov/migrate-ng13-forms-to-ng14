063466a220b8ed06bd6e733fa3cca2caaeb7c40a
Georgi Parlakov <gparlakov@gmail.com>
2022-06-11 20:47:52 +0300

    add the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'fty-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'node-3c7jnu';
+  title = 'forms-typed';
 }
